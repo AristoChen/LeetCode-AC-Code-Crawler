bb6e08457e9e474808125c0366bd0d32337b14a0
AristoChen <jj251510319013@gmail.com>
2022-02-23 12:03:27 +0800

    Only support python3 now

--- setup.py
+++ setup.py
@@ -1,28 +1,14 @@
 from setuptools import setup
-import sys
 
-if sys.version_info < (3, 0):
-    setup(
-        name = 'LeetCode AC Code Crawler',
-        version = '0.2',
-        description = 'Saved all accepted code on LeetCode',
-        classifiers = [
-            'Natural Language :: English',
-            'Programming Language :: Python :: 2.7',
-        ],
-        author = 'AristoChen',
-        install_requires = ['bs4', 'beautifulsoup', 'selenium']
-    )
-else:
-    setup(
-        name = 'LeetCode AC Code Crawler',
-        version = '0.2',
-        description = 'Saved all accepted code on LeetCode',
-        classifiers = [
-            'Natural Language :: English',
-            'Programming Language :: Python :: 2.7',
-        ],
-        author = 'AristoChen',
-        install_requires=['bs4', 'beautifulsoup4',
-                          'selenium', 'chromedriver-autoinstaller', 'certifi']
-    )
+setup(
+    name = 'LeetCode AC Code Crawler',
+    version = '0.3',
+    description = 'Saved all accepted code on LeetCode',
+    classifiers = [
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+    ],
+    author = 'AristoChen',
+    install_requires=['bs4', 'beautifulsoup4',
+                        'selenium', 'chromedriver-autoinstaller', 'certifi']
+)
